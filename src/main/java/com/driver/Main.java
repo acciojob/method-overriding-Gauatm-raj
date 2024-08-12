46e1bd4512bacd0868ed60b30566e1bdfc7a1277
Gauatm-raj <gautamraj7569@gmail.com>
2024-08-12 23:01:17 +0530

    Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        // Creating an object of class B
+        B objB = new B();
+
+        // Calling the meth method from class A (inherited by B)
+        System.out.println(objB.meth());  // Output: "Invoking method from class A"
+    }
   
 }
