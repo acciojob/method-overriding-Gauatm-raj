6a33428202eaebe8cd0712ebe7772db9721faaab
Gauatm-raj <gautamraj7569@gmail.com>
2024-08-12 23:09:50 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,11 +2,14 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        // Creating an object of class B
+        // Task 3: Create an object of B and call meth
         B objB = new B();
-
-        // Calling the meth method from class A (inherited by B)
-        System.out.println(objB.meth());  // Output: "Invoking method from class A"
+//        System.out.println(objB.meth());  // Output: "Invoking method from class A"
+//
+//        // Task 5: Call the overridden method
+//        System.out.println(objB.meth());  // Output: "Method is overridden in Extended class B"
+        String ans= objB.meth();
     }
-  
+
+
 }
